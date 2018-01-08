2f7dd2bbab03e95a99ad6ed69e0dec83232ad49f
Stephanie Liu <stephaniel96@gmail.com>
2018-01-08 05:54:45 -0500

    path change

--- data_batch.py
+++ data_batch.py
@@ -53,7 +53,7 @@ def replaceZeroes(data):
 def split_data():
     """ Split data so that training and testing do not overlap """
     # path = "../../clean_data/mat_normalized/*.mat"
-    path = "/mat_normalized/*.mat"
+    path = "mat_normalized/*.mat"
     list_files = glob.glob(path)
     length = len(list_files)
     train_files = list_files[:length-100]
